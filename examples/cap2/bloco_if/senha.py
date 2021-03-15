7c20fb4e2625ce73ce86c693a811c65194c168c4
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-03-15 16:03:39 -0300

    Exemplo de utilização do bloco if

---
+++ examples/cap2/bloco_if/senha.py
@@ -0,0 +1,11 @@
+login_correto='Dirack'
+senha_correto='12345'
+
+print("Digite o login")
+login = input()
+
+print("Digite a senha")
+senha = input()
+
+if senha == senha_correto:
+	print("login realizado com sucesso!")
